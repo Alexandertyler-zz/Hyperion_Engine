1cfebe7e9b09e322a25613c71c17bf089409f009
Alex Tyler <at@u10-2-32-118.office.norse-data.com>
2015-06-18 14:17:06 -0700

    Started 3d math library with vector types.

---
+++ 3dMath/hypE_3dMath_Vec.hpp
@@ -0,0 +1,25 @@
+#ifndef HYPE_3DMATH_VEC_H
+#define HYPE_3DMATH_VEC_H
+
+/*
+ * Vector Class
+ */
+
+template<class VEC_TYPE, unsigned SIZE>
+
+class Vector
+{
+	public:
+		typedef VEC_TYPE vec_type;
+		int vec_size = SIZE;
+		vec_type vec_vals[size+1];
+		
+
+		
+
+		Vector()
+		{
+		
+		}
+
+};
